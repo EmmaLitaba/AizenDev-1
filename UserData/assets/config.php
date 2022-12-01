e736d997933ba1a171741aad46d3a48681da1a11
EmmaLitaba <97298226+EmmaLitaba@users.noreply.github.com>
2022-12-01 15:56:31 +0200

    Add files via upload

---
+++ UserData/assets/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Names
+define("BANKNAME", "Mapurisa Bank");
+define("EMAIL", "one.mapurisa@ac.zm");
+define("PASSWORD", "");
+define("ADDRESS", "Lusaka, University Of Zambia");
+define("MOBILENO", "+260 761234567");
+define("LOCATION_CORDINATE", "https://www.google.com/maps/place/Department+Of+Computer+Studies/@-15.3922019,28.3303368,19.5z/data=!4m12!1m6!3m5!1s0x19408b6f185a8c37:0x6db9927b2af3725a!2sThe+University+of+Zambia!8m2!3d-15.392315!4d28.3284985!3m4!1s0x19408b6e9ef4c511:0xc4a8e0ef0be7e0ee!8m2!3d-15.392006!4d28.3304381");
